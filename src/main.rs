45a470c07b0d937219b8d4f8d4b5c07df9aed855
Shane Vandegrift <vandesm14@gmail.com>
2022-11-18 00:20:06 -0500

    new code and less deps (thanks @leonskidev)

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,120 @@
-use clap::Parser;
 use std::{
-  cmp::max,
-  env::{self, Args},
-  fs,
-  io::{self, Write},
+  io::{self, BufRead, BufReader, Write},
+  path::PathBuf,
 };
-use terminal_size::{terminal_size, Height, Width};
 
-fn main() {
-  let file_path = env::args().nth(1).unwrap_or("history.txt".to_string());
+use core::fmt;
 
-  let mut history: Vec<String> = Vec::new();
+// NOTE: you can give `main()` a return value which will just be `unwrap()`
+// internally, which is very nice for testing out stuff
+fn main() -> io::Result<()> {
+  // NOTE: since `"history.txt".to_string()` is expensive, you only want to do
+  // it if necessary, so we use `unwrap_or_else` which takes in a closure
+  // instead of the value iteself
+  let history_file_path = PathBuf::from(
+    std::env::args()
+      .nth(1)
+      .unwrap_or_else(|| "history.txt".to_string()),
+  );
 
-  if let Ok(contents) = fs::read_to_string(&file_path) {
-    history = contents.split('\n').map(|s| s.to_string()).collect();
-  }
+  let history_file =
+    std::fs::File::open(&history_file_path).map(BufReader::new)?;
 
-  loop {
-    clear();
-    display(&history);
-    prompt();
+  // NOTE: unfortunately `try_collect()` is yet stable, so i have to manually do
+  // it using `try_fold()` which makes this look a little messier
+  let history =
+    history_file
+      .lines()
+      .try_fold(Vec::new(), |mut history, line| {
+        history.push(line?);
+        Ok::<_, io::Error>(history)
+      })?;
 
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
+  let mut stdout = io::stdout();
+  let mut session = Session::new(history, 0);
 
-    match input.as_str().trim() {
-      "exit" => break,
-      _ => (),
+  clear_and_position();
+  println!("{}", session);
+  prompt(&mut stdout)?;
+
+  for line in io::stdin().lines() {
+    let mut line = line?.trim().to_string();
+
+    // commands
+    if line.starts_with('/') {
+      line.remove(0);
+      match line.to_lowercase().as_str() {
+        "e" | "exit" => break,
+        "u" | "up" => session.up(1),
+        "d" | "down" => session.down(1),
+        _ => (),
+      }
+    } else {
+      session.add(line);
     }
 
-    history.push(input.trim().to_string());
+    clear_and_position();
+    println!("{}", session);
+    prompt(&mut stdout)?;
+  }
 
-    fs::write(&file_path, history.join("\n")).unwrap();
+  Ok(())
+}
+
+#[derive(Debug)]
+struct Session {
+  history: Vec<String>,
+  offset: usize,
+}
+
+impl Session {
+  #[inline]
+  const fn new(history: Vec<String>, offset: usize) -> Self {
+    Self { history, offset }
+  }
+
+  fn up(&mut self, amount: usize) {
+    self.offset = self.offset.saturating_add(amount).min(self.history.len());
+  }
+
+  fn down(&mut self, amount: usize) {
+    self.offset = self.offset.saturating_sub(amount).max(0);
+  }
+
+  fn add(&mut self, line: String) {
+    self.history.push(line);
   }
 }
 
-fn prompt() {
-  print!("> ");
-  io::stdout().flush().unwrap();
+impl fmt::Display for Session {
+  fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+    let (cols, rows) = termion::terminal_size().map_err(|_| fmt::Error)?;
+
+    let empty_space = (rows as usize).saturating_sub(self.history.len() + 3);
+    write!(f, "{}", "\n".repeat(empty_space))?;
+
+    // NOTE: using a `try_for_each()` to catch errors is much nicer since it
+    // cuts down on code
+    self
+      .history
+      .iter()
+      .enumerate()
+      .rev()
+      .skip(self.offset)
+      .take(rows as usize - 3)
+      .rev()
+      .try_for_each(|(i, line)| writeln!(f, "{}: {}", i, line))?;
+
+    writeln!(f, "{}", "─".repeat(cols as usize))?;
+    write!(f, "OFFSET: {}", self.offset)
+  }
 }
 
-fn clear() {
-  print!("\x1B[2J\x1B[1;1H");
+fn clear_and_position() {
+  println!("{}{}", termion::clear::All, termion::cursor::Goto(1, 1));
 }
 
-fn display(history: &Vec<String>) {
-  let width = terminal_size().map(|(Width(w), _)| w).unwrap_or(80) as usize;
-  let height = terminal_size().map(|(_, Height(h))| h).unwrap_or(24) as usize;
-
-  let empty_space =
-    max(0, height.saturating_sub(2).saturating_sub(history.len()));
-
-  for string in history.iter().rev().take(height - 2).rev() {
-    println!("{}", string);
-  }
-
-  for _ in 0..empty_space {
-    println!();
-  }
-
-  println!("{}", "─".repeat(width));
+fn prompt(stdout: &mut io::Stdout) -> io::Result<()> {
+  stdout.write_all(b"> ")?;
+  stdout.flush()
 }
